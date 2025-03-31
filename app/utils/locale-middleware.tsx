c30d21d3e0ca69fa38462b3f05cb1bbd94d9c877
Igor Szymański <igor.szymanski@me.com>
2025-03-31 10:34:29 +0200

    Updated dependencies. Fixed problem with local-middleware where incorrect language would be inferred during the first render.

--- app/utils/locale-middleware.tsx
+++ app/utils/locale-middleware.tsx
@@ -1,12 +1,46 @@
-import { createMiddleware } from "@tanstack/react-start";
 import { AsyncLocalStorage } from 'node:async_hooks'
-import { baseLocale, getLocale, Locale, overwriteGetLocale } from "~/paraglide/runtime.js";
+
+import { createMiddleware } from '@tanstack/react-start'
+import { getWebRequest } from '@tanstack/react-start/server'
+
+import { baseLocale, getLocale, type Locale, overwriteGetLocale } from '~/paraglide/runtime.js'
+import { paraglideMiddleware } from '~/paraglide/server.js'
 
 export const localeMiddleware = createMiddleware()
-  .client((context) => {
-    return context.next({ sendContext: { locale: getLocale() } }) })
+  .client(async (context) => {
+    try {
+      const request = getWebRequest()
+
+      if (!request) {
+        return context.next({
+          sendContext: {
+            locale: baseLocale as Locale,
+          },
+        })
+      }
+
+      const resolvedLocale = await new Promise<Locale>((resolve) =>
+        paraglideMiddleware(request, async ({ locale }) => {
+          resolve(locale)
+        }),
+      )
+
+      return context.next({
+        sendContext: {
+          locale: resolvedLocale,
+        },
+      })
+    } catch {
+      return context.next({
+        sendContext: {
+          locale: getLocale(),
+        },
+      })
+    }
+  })
   .server(async (context) => {
     const storage = new AsyncLocalStorage<Locale>()
     overwriteGetLocale(() => storage.getStore() ?? baseLocale)
+
     return await storage.run(context.context.locale, async () => await context.next())
-  });
+  })
