3ff137f6b49d42505e11be98224ad411bb23162e
Igor Szymański <igor.szymanski@me.com>
2025-06-15 06:28:02 +0200

    fix: update middleware registration in global middleware and server function

--- src/serverFunctions/get-server-message.ts
+++ src/serverFunctions/get-server-message.ts
@@ -1,7 +1,9 @@
 import { createServerFn } from "@tanstack/react-start";
 import { m } from "~/paraglide/messages.js";
+import { localeMiddleware } from '~/utils/locale-middleware'
 
 export const getServerMessage = createServerFn()
+  .middleware([localeMiddleware])
   .validator((emoji: string) => emoji)
   .handler((ctx) => {
     return m.server_message({ emoji: ctx.data });
