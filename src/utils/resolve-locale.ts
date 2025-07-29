afa7c94e44f4172378acb37b449a72e72e63a318
Júlio Mühlbauer <juliowernermm@gmail.com>
2025-07-29 18:26:07 -0300

    working!

--- src/utils/resolve-locale.ts
+++ src/utils/resolve-locale.ts
@@ -1,19 +1,10 @@
 import { createIsomorphicFn } from "@tanstack/react-start";
 import { getWebRequest } from "@tanstack/react-start/server";
-import { baseLocale, getLocale, Locale } from "~/paraglide/runtime.js";
-
-import { paraglideMiddleware } from "~/paraglide/server.js";
+import {
+  extractLocaleFromRequestAsync,
+  getLocale,
+} from "~/paraglide/runtime.js";
 
 export const resolveLocale = createIsomorphicFn()
   .client(getLocale)
-  .server(() => {
-    const request = getWebRequest();
-
-    if (!request) {
-      return baseLocale;
-    }
-
-    return new Promise<Locale>((resolve) =>
-      paraglideMiddleware(request, ({ locale }) => resolve(locale)),
-    );
-  });
+  .server(() => extractLocaleFromRequestAsync(getWebRequest()));
