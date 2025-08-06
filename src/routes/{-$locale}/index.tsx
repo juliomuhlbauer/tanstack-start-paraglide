918d596b592f300eeb62aed9a43f074d1fbafd97
Júlio Mühlbauer <juliowernermm@gmail.com>
2025-08-06 17:13:05 -0300

    locale from loader

--- src/routes/{-$locale}/index.tsx
+++ src/routes/{-$locale}/index.tsx
@@ -1,18 +1,27 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { m } from "~/paraglide/messages.js";
-import { locales, setLocale } from "~/paraglide/runtime.js";
+import { getLocale, locales, setLocale } from "~/paraglide/runtime.js";
 import { getServerMessage } from "~/serverFunctions/get-server-message";
 
 export const Route = createFileRoute("/{-$locale}/")({
   component: Home,
-  loader: () => getServerMessage({ data: "📩" }),
+  loader: () => {
+    return {
+      localeFromLoader: getLocale(),
+      messageFromLoader: m.example_message({ username: "John Doe" }),
+      serverFunctionMessage: getServerMessage({ data: "📩" }),
+    };
+  },
 });
 
 function Home() {
-  const serverMessage = Route.useLoaderData();
+  const { serverFunctionMessage, messageFromLoader, localeFromLoader } =
+    Route.useLoaderData();
   return (
     <div className="p-2">
-      <h2>{serverMessage}:</h2>
+      <h2>Locale from loader: {localeFromLoader}</h2>
+      <h2>Message from loader: {messageFromLoader}</h2>
+      <h2>Server function message: {serverFunctionMessage}:</h2>
       <h3>{m.example_message({ username: "John Doe" })}</h3>
       {locales.map((locale) => (
         <button
